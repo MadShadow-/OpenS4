65282ac838a14e62e0122a9da2d87451abbda08e
MadShadow- <Mad.Shadow@web.de>
2020-07-29 02:08:41 +0200

    Added read_area to binary reader

--- OpenS4/Import/Reader.cpp
+++ OpenS4/Import/Reader.cpp
@@ -2,44 +2,58 @@
 
 #include <fstream>
 
-namespace OpenS4::Import {
-Reader::Reader(std::string file) {
-    auto is = std::ifstream(file, std::ios::binary);
+namespace OpenS4::Import
+{
+    Reader::Reader(std::string file)
+    {
+        auto is = std::ifstream(file, std::ios::binary);
 
-    if (is) {
-        is.seekg(0, is.end);
-        int length = is.tellg();
-        is.seekg(0, is.beg);
+        if (is)
+        {
+            is.seekg(0, is.end);
+            int length = is.tellg();
+            is.seekg(0, is.beg);
 
-        unsigned char* buffer = new unsigned char[length];
+            unsigned char* buffer = new unsigned char[length];
 
-        is.read((char*)buffer, length);
+            is.read((char*)buffer, length);
 
-        m_data = std::vector<unsigned char>(buffer, buffer + length);
+            m_data = std::vector<unsigned char>(buffer, buffer + length);
 
-        delete[] buffer;
+            delete[] buffer;
+        }
+
+        is.close();
     }
 
-    is.close();
-}
+    Reader::Reader(std::vector<uint8_t> data) { m_data = data; }
 
-Reader::Reader(std::vector<uint8_t> data) { m_data = data; }
-
-uint32_t Reader::read_int32(uint32_t offset) {
-    uint8_t b[4] = {0};
-    for (int i = 0; i < 4; i++) {
-        b[i] = m_data.at(offset++);
+    uint32_t Reader::read_int32(uint32_t offset)
+    {
+        uint8_t b[4] = {0};
+        for (int i = 0; i < 4; i++)
+        {
+            b[i] = m_data.at(offset++);
+        }
+        return *(uint32_t*)&b[0];
     }
-    return *(uint32_t*)&b[0];
-}
 
-uint8_t Reader::read_byte(uint32_t offset) { return m_data.at(offset); }
+    uint8_t Reader::read_byte(uint32_t offset) { return m_data.at(offset); }
 
-uint16_t Reader::read_word(uint32_t offset) {
-    uint8_t b[2] = {0};
-    for (int i = 0; i < 2; i++) {
-        b[i] = m_data.at(offset++);
+    uint16_t Reader::read_word(uint32_t offset)
+    {
+        uint8_t b[2] = {0};
+        for (int i = 0; i < 2; i++)
+        {
+            b[i] = m_data.at(offset++);
+        }
+        return *(uint16_t*)&b[0];
     }
-    return *(uint16_t*)&b[0];
-}
-}  // namespace OpenS4::Graphics
+
+    uint8_t* Reader::read_area(uint32_t offset, size_t size)
+    {
+        uint8_t* pData = new uint8_t[size];
+        memcpy_s(pData, size, m_data.data() + offset, size);
+        return pData;
+    }
+}  // namespace OpenS4::Import
--- OpenS4/Import/Reader.hpp
+++ OpenS4/Import/Reader.hpp
@@ -19,6 +19,8 @@ class Reader {
     uint8_t read_byte(uint32_t offset);
     uint16_t read_word(uint32_t offset);
 
+    uint8_t* read_area(uint32_t offset, size_t size);
+
     size_t size() { return m_data.size(); }
 };
 }  // namespace OpenS4::Graphics
